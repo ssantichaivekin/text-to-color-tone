9ae7253429bbc3dfdbcaf6aef3675398b83c9967
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-05-19 22:37:44 -0700

    Add Google search engine id.

--- get_image_links.py
+++ get_image_links.py
@@ -6,4 +6,16 @@ thumbnails.
 
 import requests
 
-def 
+def read_key(keyfilename='google-api-key.txt') :
+    '''
+    Read the Google API key from keyfilename.
+    '''
+    key = open(keyfilename).read()
+    return key
+
+def get_image_links(key, image_num=6) :
+    '''
+    Get a list of thumbnail links using the google API
+    key.
+    '''
+    param = {}

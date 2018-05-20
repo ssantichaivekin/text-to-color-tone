8710051bd9520b1ecb5cb8c83604ce77e9be683d
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-05-20 02:41:40 -0700

    Finished get_image_links.py

--- get_image_links.py
+++ get_image_links.py
@@ -5,6 +5,7 @@ thumbnails.
 '''
 
 import requests
+from bs4 import BeautifulSoup
 
 def read_key(keyfilename='google-api-key.txt') :
     '''
@@ -13,9 +14,42 @@ def read_key(keyfilename='google-api-key.txt') :
     key = open(keyfilename).read()
     return key
 
-def get_image_links(key, image_num=6) :
+def read_search_engine_id(keyfilename='google-engine-name.txt') :
+    '''
+    Read the custom search engine.
+    '''
+    key = open(keyfilename).read()
+    return key
+
+def get_image_links(query, image_num=6) :
     '''
     Get a list of thumbnail links using the google API
     key.
     '''
-    param = {}
+    key = read_key()
+    engine_id = read_search_engine_id()
+    s_type = 'photo'
+    params = {
+        # search query; the text to find color tone
+        'q'         : query, 
+        # Google API search engine ID
+        'cx'        : engine_id,
+        # Image size : small, medium, large, etc
+        'imgSize'   : 'medium',
+        # Image type : clipart, face, lineart, news, and photo.
+        'imgType'   : 'photo',
+        # Search type -- we are searching images
+        'searchType': 'image',
+        # 6 seems to be enough for finding color tone.
+        'num'       : 6,
+        # This is my Google API key.
+        'key'       : key
+        }
+    link = 'https://www.googleapis.com/customsearch/v1'
+    r = requests.get(link, params)
+    thumbnail_links = []
+    for search_item in r.json()['items'] :
+        thumbnail_link = search_item['image']['thumbnailLink']
+        # print(thumbnail_link)
+        thumbnail_links += [thumbnail_link]
+    return thumbnail_links

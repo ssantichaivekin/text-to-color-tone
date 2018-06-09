7087211209aa9c2c93e40a140b894cafd2dca6aa
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-06-09 16:05:23 -0700

    Finish seperating words into sets.

--- writer/segment_word.py
+++ writer/segment_word.py
@@ -1,20 +1,34 @@
 from nltk.corpus import cmudict
+from nltk.corpus import wordnet
 
-# get CMU pronouncing dictionary
-dictionary = cmudict.dict()
+# get pronounciation dictionary
+pronun_word_list = list(cmudict.dict().keys())
+# get definition dictionary
+def_word_list= [ word for word in wordnet.words() ]
+
+def get_all_words() :
+    '''
+    We want words that appear on both dict.
+    '''
+    all_words = list(set(pronun_word_list + def_word_list))
+    all_words = list(filter(str.isalpha, all_words))
+    all_words = sorted(all_words)
+    return all_words
+
 
 def write_chunks() :
     '''
     Write all chunks into a seperate file.
     '''
-    words = (list)(dictionary.keys())
+    words = get_all_words()
     for start in range(0, len(words), 1000) :
         wordset = words[start:start+1000]
         filename = 'wordset-%d.txt' % start
         pathname = './writer/wordset/'
         fullname = pathname + filename
         with open(fullname, 'w') as f :
-            f.writelines(wordset)
+            for word in wordset :
+                f.write('%s\n' % word)
 
 def read_chunks(chunk_i) :
     '''

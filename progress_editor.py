e4b31357fcb0b8ca1b236f4530ee7eaead975673
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-06-09 17:05:02 -0700

    Create progress library.

---
+++ progress_editor.py
@@ -0,0 +1,29 @@
+'''
+This file defines functions that help you read and write progress.
+'''
+
+def init_progress(progressnamelist, initmsg='init') :
+    '''
+    write the progress file.
+    '''
+
+    # return list of profressname and its corresponding message.
+
+def all_progress() :
+    '''
+    Return a list of all progress names.
+    '''
+
+def read_progress(progressname) :
+    '''
+    read the msg from a progress name
+    '''
+
+def get_next_process() :
+    '''
+    Return the first progressname that is not finished
+    '''
+
+def write_progress(progressname, msg) :
+    # write the progress to the progress name
+

50d8534c0d71d2545e4ac36d818c06b1939aad0d
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-06-09 23:28:55 -0700

    Finish init_progress

--- progress_editor.py
+++ progress_editor.py
@@ -6,24 +6,40 @@ def init_progress(progressnamelist, initmsg='init') :
     '''
     write the progress file.
     '''
-
-    # return list of profressname and its corresponding message.
+    with open('progress.txt', 'w') as f :
+        for progressname in progressnamelist :
+            print('%s:%s' % (progressname, initmsg), file=f)
 
 def all_progress() :
     '''
     Return a list of all progress names.
     '''
+    return
 
 def read_progress(progressname) :
     '''
     read the msg from a progress name
     '''
+    return
+
+def log_error(progressname, error) :
+    '''
+    Log an error from exception.
+    '''
+    with open('errorlog.txt', 'w') as f :
+        print(progressname, file=f)
+        print(error, file=f)
 
 def get_next_process() :
     '''
     Return the first progressname that is not finished
     '''
+    return
 
 def write_progress(progressname, msg) :
     # write the progress to the progress name
+    return
 
+if __name__ == '__main__' :
+    dummylist = ['dummy-%d' % i for i in range(10)]
+    init_progress(dummylist)
